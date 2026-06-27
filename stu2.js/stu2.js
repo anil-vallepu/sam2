e60ce7dbb79fb796675fe46ee8401055a90a9566
Deep Thanush <deepthanush07@gmail.com>
2026-06-27 14:52:51 +0530

    added new product to the shopping

--- stu2.js/stu2.js
+++ stu2.js/stu2.js
@@ -2,4 +2,8 @@ let cart =[];
 function addToCart(name,price){
 let item = cart.find(product =>product.name ===name);
 if(item){item.quantity++;}
-	else{
+	else{cart.push({
+		name:name,
+		price:price,
+		qty:1{);
+
